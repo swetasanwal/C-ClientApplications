055ea27acf564f68f2190c6f2c0853087c9745d7
Sanwal <ssanwal@amazon.com>
2018-03-06 22:06:51 -0800

    Assignment 2

---
+++ csharp220/tree/master/homework2_new/homework2/Models/User.cs
@@ -0,0 +1,129 @@
+﻿using System.ComponentModel;
+namespace homework2.Models
+{
+    class User : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string name = string.Empty;
+        private string password = string.Empty;
+        private string nameError;
+        private string passwordError;
+
+        public string NameError
+        {
+            get
+            {
+                return nameError;
+            }
+            set
+            {
+                if (nameError != value)
+                {
+                    nameError = value;
+                    OnPropertyChanged("NameError");
+                }
+            }
+        }
+
+        public string PasswordError
+        {
+            get
+            {
+                return passwordError;
+            }
+            set
+            {
+                if(passwordError != value)
+                {
+                    passwordError = value;
+                    OnPropertyChanged("PasswordError");
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        // IDataErrorInfo interface
+        public string Error
+        {
+            get
+            {
+                return NameError;
+            }
+        }
+
+        // IDataErrorInfo interface
+        // Called when ValidatesOnDataErrors=True
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case "Name":
+                        {
+                            NameError = "";
+                            if (string.IsNullOrEmpty(Name))
+                            {
+                                NameError = "Name cannot be empty.";
+                            }
+                            if (Name.Length > 12)
+                            {
+                                NameError = "Name cannot be longer than 12 characters.";
+                            }
+                            return NameError;
+                        }
+                    case "Password":
+                        {
+                            PasswordError = "";
+                            if (string.IsNullOrEmpty(Password))
+                            {
+                                PasswordError = "Password cannot be empty";
+                            }
+                            return PasswordError;
+                        }
+                }
+                return string.Empty;
+            }
+        }
+
+        // INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ csharp220/tree/master/homework2_new/homework2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            //Display to the console, all the passwords that are "hello"
+            var helloPassword = from user in users
+                                where (user.Password == "hello")
+                                select user;
+
+            Console.WriteLine("Following are the names of the user with hello password:");
+            foreach (var element in helloPassword)
+            {
+                Console.WriteLine($" {element.Name}");
+            }
+
+
+            //Delete any passwords that are the lower-cased version of their Name. 
+            users.RemoveAll(r => r.Password == r.Name.ToLower());
+
+            //Delete the first User that has the password "hello"
+            var firstUserWithHelloPassword = users.First(t => t.Password == "hello");
+
+            users.Remove(firstUserWithHelloPassword);
+
+            //Display to the console the remaining users with their Name and Password
+            Console.WriteLine();
+            Console.WriteLine("Following are the users left in the list:");
+            foreach (var ele in users)
+            {
+                Console.Write($" {ele.Name} {ele.Password}");
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}

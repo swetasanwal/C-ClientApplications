0322488a545870920a596885b851d249aa03fb4d
Sanwal <ssanwal@amazon.com>
2018-03-11 17:52:35 -0700

    Homework 3 related to list sorting

---
+++ csharp220/tree/master/Assignment3/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+
+            uxList.ItemsSource = users;
+        }
+
+        
+        void GridViewColumnHeaderClickedHandler(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction = ListSortDirection.Descending;
+
+            if(headerClicked != null)
+            {
+                direction = ListSortDirection.Ascending;
+            }
+
+            string header = headerClicked.Column.Header as string;
+            Sort(header, direction);
+
+        }
+
+        private void Sort(string sortBy, ListSortDirection direction)
+        {
+            ICollectionView dataView = CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+
+            dataView.SortDescriptions.Clear();
+            SortDescription sd = new SortDescription(sortBy, direction);
+            dataView.SortDescriptions.Add(sd);
+            dataView.Refresh();
+        }
+
+        private void uxList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
